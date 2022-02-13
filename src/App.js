17c167a7e97a3f3030c8d8f774a0a05bc58b7aba
DESKTOP-09V3N5C\kimsanghyeon <sanghyeon2420@kakao.com>
2022-02-13 22:40:46 +0900

    각 컴포넌트 css 적용 및 create 기능 구현중

--- src/App.js
+++ src/App.js
@@ -3,8 +3,12 @@ import './App.css';
 import Template from './components/Template';
 import Todolist from './components/Todolist';
 import { useState } from 'react';
+import { MdAddCircle } from 'react-icons/md';
+import TodoInsert from './components/TodoInsert';
 
 const App = () => {
+  const [insertToggle, setInsertToggle] = useState(false);
+
   const [todos, setTodos] = useState([
     {
       id: 1,
@@ -14,7 +18,7 @@ const App = () => {
     {
       id: 2,
       text: "할일 2",
-      checked: true
+      checked: false
     },
     {
       id: 3,
@@ -23,9 +27,15 @@ const App = () => {
     }
   ]);
 
+  const onInsertToggle = () => {
+    setInsertToggle(prev => !prev);
+  };
+
   return (
-    <Template>
-      <Todolist todos={todos}>간단한 컴포넌트</Todolist>
+    <Template todoLength={todos.length}>
+      <Todolist todos={todos}></Todolist>
+      <div className="add-todo-button" onClick={onInsertToggle}><MdAddCircle/></div>
+      {insertToggle && <TodoInsert onInsertToggle={onInsertToggle} />}
     </Template>
   );
 }
--- src/components/Todolist.js
+++ src/components/Todolist.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import TodoItem from './TodoItem';
+import './Todolist.css';
 
 const Todolist = ({todos}) => {
     return (
-            <div>
+            <div className='Todolist'>
                 {todos.map(todo => (
                     <TodoItem todo={todo} key={todo.id}></TodoItem>
                 ))}
